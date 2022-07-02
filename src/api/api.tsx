c2d420937291669e6070f4eeb43e557258098772
Shahid <shahid.alam57@gmail.com>
2022-07-02 23:24:49 +0530

    added released

---
+++ src/api/api.tsx
@@ -0,0 +1,12 @@
+const released = {
+    method: 'GET',
+    url: 'https://shazam.p.rapidapi.com/charts/track',
+    params: {locale: 'en-US', pageSize: '20', startFrom: '0'},
+    headers: {
+      'X-RapidAPI-Key': '7fedfc74f4msha589398dacdd232p1304efjsncbead375de30',
+      'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+    }
+  };
+
+
+  export {released}
--- src/components/Home/Released.tsx
+++ src/components/Home/Released.tsx
@@ -1,14 +1,47 @@
-import React from 'react'
-
+import React, { useEffect, useState } from 'react'
+import { released } from '../../api/api'
+import axios from 'axios'
 const Released = () => {
+  const [list,setList] = useState(null)
+  const fetchReleased = async()=>{
+    try{
+      const {data:{tracks}} = await axios.request(released)
+      let list = tracks.map((obj: any)=>{
+        console.log(obj);
+        
+        return <div key={obj.key} className='flex flex-col items-center'>
+          <img src={obj.images.coverart} alt="" className='w-[60%] rounded-lg ' />
+          <p>{obj.title}</p>
+        </div>
+        
+      })
+      setList(list)
+      
+    }
+    catch(error){
+      console.log(error)
+    }
+  }
+  useEffect(()=>{
+    fetchReleased()
+
+    
+  },[])
   return (
-    <div>
+    <section>
         <div className='flex flex-row'>
         <h6 className='text-xl'>Released </h6>
         <hr />
         </div>
-    </div>
+      <div className=' h-[30rem] overflow-scroll flex flex-col items-center'>
+{list}
+      </div>
+    </section>
   )
 }
 
 export default Released
+
+function aync<T>() {
+  throw new Error('Function not implemented.');
+}
