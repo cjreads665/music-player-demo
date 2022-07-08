36ddca7de30f3c9847ab94019ae7e29a2d7b5ace
Shahid <shahid.alam57@gmail.com>
2022-07-08 09:35:22 +0530

    added icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,16 +9,20 @@ const Navbar = (props: any) => {
       } md:w-[10%] md:p-[5rem] md:flex justify-center items-center z-[10] md:block w-[50%] transition-all bg-[#323fd1] max-h-[100vh]`}
     >
       <ul className="h-[50%] flex flex-col justify-evenly items-center mt-[49%] text-white ">
-        <li>
+        <li className="flex items-center">
+        <i className="fa-solid fa-house"></i>
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="flex items-center">
+        <i className="fa-solid fa-magnifying-glass"></i>
           <Link to="/search">Search</Link>
         </li>
-        <li>
+        <li className="flex items-center">
+        <i className="fa-solid fa-heart"></i>
           <Link to="/favourites">Favourites</Link>
         </li>
-        <li>
+        <li className="flex items-center">
+        <i className="fa-solid fa-list-check"></i>
           <Link to="/playlist">Playlist</Link>
         </li>
       </ul>
